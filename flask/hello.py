2ae88e9f7ab747f8136efb261fe8580982301b5f
adg3748 <revivedtomorrow@gmail.com>
2018-03-18 20:47:45 +0900

    Flask、とにかく動く状態で保存

--- flask/hello.py
+++ flask/hello.py
@@ -1,10 +1,32 @@
+# coding: UTF-8
+
 from flask import Flask, render_template
+import pymysql
+
 app = Flask(__name__) # 何してる
 
 @app.route('/') # ルーティング設定
 def hello():
-    name = "Hoge"
-    return render_template('hello.html', title='flask test', name=name) # render_template('ビュー',引数)
+
+    # db setting
+#    db = pymysql.connect(
+#            host = 'localhost',
+#            user = 'root',
+#            password = 'root',
+#            db = 'testdb',
+#            charset = 'utf8',
+#            cursorclass = pymysql.cursors.DictCursor,
+#        )
+#
+#    cur = db.cursor()
+#    sql = "select * from members"
+#    cur.execute(sql)
+#    members = cur.fetchall()
+#
+#    cur.close()
+#    db.close()
+
+    return render_template('hello.html', title='flask test') # render_template('ビュー',引数)
 
 @app.route('/good')
 def good():
